7fd8d0e02103248110e78cc37a7b9cb772323efe
Git-Admin <euro@profires.cz>
2016-02-24 21:35:44 +0100

    Init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fiopython',
+      version='0.1',
+      description='The FIO bank a.s. API connector',
+      url='https://git.profires.cz/open-source/fiopython',
+      author='Martin Miksanik',
+      author_email='martin@miksanik.net',
+      license='MIT',
+      packages=['fiopython'],
+      zip_safe=False)
